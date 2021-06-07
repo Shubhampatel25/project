e17e690cf0ff72745de026ae4bcbd6ba269b244f
Shubhampatel25 <73432071+Shubhampatel25@users.noreply.github.com>
2021-06-07 12:54:22 +0530

    style image

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,8 +2,7 @@ class Property < ApplicationRecord
 	has_one_attached :photo
 
 	def thumbnail
-		return self.photo.variant(resize: '50x50').processed
+		return self.photo.variant(resize: '200x200')
 	end	
 
-	scope :latest, -> { order created_at: :desc } 
 end
