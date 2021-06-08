16962dad21f742633eeadf5e07ba3cba32a41658
Shubhampatel25 <73432071+Shubhampatel25@users.noreply.github.com>
2021-06-08 12:02:51 +0530

    resize the show image

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,7 @@ class Property < ApplicationRecord
 	has_one_attached :photo
 
 	def thumbnail
-		return self.photo.variant(resize: '200x200')
+		return self.photo.variant(resize: '1200x300!').processed
 	end	
 
 end
